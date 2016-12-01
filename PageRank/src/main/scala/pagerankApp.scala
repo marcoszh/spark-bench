85bb2242f676346073a024f97dbb674be7bd9bc9
ZHANG Chengliang <czhangbn@connect.ust.hk>
2016-12-01 17:59:55 +0800

    master

--- PageRank/src/main/scala/pagerankApp.scala
+++ PageRank/src/main/scala/pagerankApp.scala
@@ -35,7 +35,7 @@ import org.apache.spark.graphx.impl.{EdgePartitionBuilder, GraphImpl}
 import scala.collection.parallel._
 
 object pagerankApp extends Logging {
-  var masterurl = "ec2-54-144-198-128.compute-1.amazonaws.com"
+  var masterurl = "ec2-54-211-21-101.compute-1.amazonaws.com"
   var hdfs = "hdfs://" + masterurl + ":9000/SparkBench/"
 
   def main(args: Array[String]) {
